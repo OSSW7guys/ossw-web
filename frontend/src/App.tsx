205082c2771d6069e3b1f16df8adcdbe64704092
SeungyeonHa <0727ha@naver.com>
2025-05-25 17:30:34 +0900

    page 구성 세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css'
+import './App.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import NotFoundPage from './pages/NotFoundPage';
+import MainPage from './pages/MainPage';
+import CheckPage from './pages/CheckPage';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage/>,
+      errorElement: <NotFoundPage/>,
+      children:[
+        {path: 'main', element: <MainPage/>},
+        {path: 'check', element: <CheckPage/>}
+      ]
+    }
+  ]);
 
 function App() {
-
   return (
-    <>
-    </>
+    <RouterProvider router={router}>
+    </RouterProvider>
   )
 }
 
